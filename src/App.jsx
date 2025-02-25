b6aae4b8a24824d38d6fe4059320ef1b1b12ae47
sathish3201 <sathishchakali1023@gmail.com>
2025-02-25 10:36:58 +0530

    this is 8 commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import EcomPort from './ecommerce/EcomPort'
 import Navbar from './portfolio/components/navbar/Navbar'
 import Footer from './portfolio/components/footer/Footer'
-
+import Login from './ecommerce/components/Login'
+import Register from './ecommerce/components/Register'
+import Contact from './portfolio/components/contact/Contact'
+import Cart from './ecommerce/components/Cart'
+import Orders from './ecommerce/components/Orders'
+import Checkout from './ecommerce/components/Checkout'
+import HomePage from './ecommerce/components/HomePage'
+import ItemDetail from './ecommerce/components/ItemDetail'
+import HomePort from './portfolio/HomePort'
+import PageNotFound from './ecommerce/components/PageNotFound'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 const App = () => {
   
   return (
@@ -10,7 +20,21 @@ const App = () => {
         <div className="nav-section">
           <Navbar/>
         </div>
-          <EcomPort />
+        <Router basename='/react-ecommerce-site/'>
+         <Routes>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+          
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/orders' element = {<Orders/>} />
+            <Route path="/checkout" element = {<Checkout/>} />
+            <Route  path='/homepage' element= {<HomePage/>}/>
+            <Route path='/item/:id' element={<ItemDetail/>}/>
+            <Route exact path='/' element = {<HomePort/>} />
+            <Route path='*' element={<PageNotFound/>}/>
+          </Routes>
+       </Router>
         <div className="footer-section"><Footer/></div>
     </div>
   )
--- src/ecommerce/EcomPort.jsx
+++ src/ecommerce/EcomPort.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import { Route, BrowserRouter as Router , Routes} from 'react-router-dom'
-import Login from './components/Login'
-import Register from './components/Register'
+// import React from 'react'
+// import { Route, BrowserRouter as Router , Routes} from 'react-router-dom'
+// import Login from './components/Login'
+// import Register from './components/Register'
 
-import HomePage from './components/HomePage'
-import ItemDetail from './components/ItemDetail'
-import Orders from './components/Orders'
-import Cart from './components/Cart'
-import Checkout from './components/Checkout'
-import HomePort from '../portfolio/HomePort'
-import PageNotFound from './components/PageNotFound'
-import Contact from '../portfolio/components/contact/Contact'
-const EcomPort = () => {
-  
-  return (
+// import HomePage from './components/HomePage'
+// import ItemDetail from './components/ItemDetail'
+// import Orders from './components/Orders'
+// import Cart from './components/Cart'
+// import Checkout from './components/Checkout'
+// import HomePort from '../portfolio/HomePort'
+// import PageNotFound from './components/PageNotFound'
+// import Contact from '../portfolio/components/contact/Contact'
+// const EcomPort = () => {
+//   // https://django-ecommerce-api-wq6l.onrender.com
+//   return (
    
-     <Router basename='/react-ecommerce-site'>
-      <Routes>
+//      <Router basename='/react-ecommerce-site'>
+//       <Routes>
       
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+//         <Route path='/login' element={<Login/>}/>
+//         <Route path='/register' element={<Register/>}/>
+//         <Route path='/contact' element={<Contact/>}/>
        
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/orders' element = {<Orders/>} />
-        <Route path="/checkout" element = {<Checkout/>} />
-        <Route  path='/homepage' element= {<HomePage/>}/>
-        <Route path='/item/:id' element={<ItemDetail/>}/>
-        <Route exact path='/' element = {<HomePort/>} />
-        <Route path='*' element={<PageNotFound/>}/>
-      </Routes>
-     </Router>
+//         <Route path='/cart' element={<Cart/>}/>
+//         <Route path='/orders' element = {<Orders/>} />
+//         <Route path="/checkout" element = {<Checkout/>} />
+//         <Route  path='/homepage' element= {<HomePage/>}/>
+//         <Route path='/item/:id' element={<ItemDetail/>}/>
+//         <Route exact path='/' element = {<HomePort/>} />
+//         <Route path='*' element={<PageNotFound/>}/>
+//       </Routes>
+//      </Router>
     
+//   )
+// }
+
+// export default EcomPort
+
+
+import React from 'react'
+
+const EcomPort = () => {
+  return (
+    <div>
+      
+    </div>
   )
 }
 
